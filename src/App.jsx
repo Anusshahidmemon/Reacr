8b2f8977f349453100f32b5dc9816b445267724f
Anusshahidmemon <shahidanus21@gmail.com>
2024-09-21 01:17:36 +0500

    static website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  Navbar from './navbar';
-import Home from "./pages/Home"
-import Pricing from "./pages/pricing"
-import About from "./pages/about"
+
 import  Carousels  from './carousel';
 import Card from "./card"
 function App() {
