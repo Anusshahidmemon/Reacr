ffa462ac82a675fb4db42621e6257bdf16cf778a
Anusshahidmemon <shahidanus21@gmail.com>
2024-09-21 10:46:31 +0500

    react-practice

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,11 @@ import  Carousels  from './carousel';
 import Card from "./card"
 function App() {
 
-  let component
-  switch (window.location.pathname) {
-  case "/":
-  component = <Home />
-  break
-  case "/pricing":
-  component = <Pricing />
-  break
-  case "/about":
-  component = <About />
-  break
-  }
+
   return (
     <>
      <Navbar/>
-     {component}
+   
 <Carousels/>
  <Card/>
     </>
